53531bc6fcdc84d819f8aacd3001d52d28c4e4e7
Oscar Daniel <71881424+Kurizen@users.noreply.github.com>
2021-05-15 00:53:54 +0700

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<?php 
+session_start();
+
+require_once "authCookieSessionValidate.php";
+
+if(!$isLoggedIn) {
+    header("Location: ./");
+}
+?>
+<style>
+.member-dashboard {
+    text-align: center;
+    padding: 40px;
+    background: #00000;
+    color: #555;
+    border-radius: 4px;
+    display: inline-block;
+}
+
+.member-dashboard a {
+    color: #09F;
+    text-decoration: none;
+}
+</style>
+<div class="member-dashboard">
+    Berhasil Masuk.<br>
+    <a href="logout.php">Logout</a><br>
+    <a href="https://jojo.fandom.com/wiki/The_World">DUNIA</a>
+</div>
